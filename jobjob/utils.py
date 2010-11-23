410dbca3e4f5b86dcdd1a17e2cf80e4ddcd4dff0
Silvio René <silvio.rene@gmail.com>
2010-11-23 19:42:31 +0100

    vars --> input_vars

--- jobjob/utils.py
+++ jobjob/utils.py
@@ -1,16 +1,16 @@
 
 
 
-def copy_input(src, dst, vars):
+def copy_input(src, dst, input_vars):
     """..."""
     with open(dst, 'w') as dst_input:
         for line in open(src):
             values = line.split()
-            if values and vars.has_key(values[0]):
+            if values and input_vars.has_key(values[0]):
                 var, old_value, comment = line.rpartition(values[1])
-                line = var + str(vars[values[0]]) + comment
-                del vars[values[0]]
+                line = var + str(input_vars[values[0]]) + comment
+                del input_vars[values[0]]
             dst_input.write(line)
         dst_input.write('\n#----\n')
-        for var, value in vars.items():
+        for var, value in input_vars.items():
             dst_input.write('%s %s\n'%(var, value))
