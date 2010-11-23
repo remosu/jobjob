f0c9904792a65a533fb874ea3c07a286d8453cc6
Silvio René <silvio.rene@gmail.com>
2010-11-23 20:01:44 +0100

    PEP8 refactoring
    HG : Enter commit message.  Lines beginning with 'HG:' are removed.

--- jobjob/utils.py
+++ jobjob/utils.py
@@ -1,16 +1,15 @@
-
-
+"""..."""
 
 def copy_input(src, dst, input_vars):
     """..."""
     with open(dst, 'w') as dst_input:
         for line in open(src):
             values = line.split()
-            if values and input_vars.has_key(values[0]):
-                var, old_value, comment = line.rpartition(values[1])
+            if values and values[0] in input_vars:
+                var, _, comment = line.rpartition(values[1])
                 line = var + str(input_vars[values[0]]) + comment
                 del input_vars[values[0]]
             dst_input.write(line)
         dst_input.write('\n#----\n')
         for var, value in input_vars.items():
-            dst_input.write('%s %s\n'%(var, value))
+            dst_input.write('%s %s\n' % (var, value))
